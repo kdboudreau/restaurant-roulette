aacd036d6393c3f1a5e660e85eb73a4dbeb01691
Kathryn <kdboudreau1@gmail.com>
2020-02-24 17:09:34 -0500

    css footer changes

--- main.js
+++ main.js
@@ -109,17 +109,3 @@ function spinTheWheel() {
         $('.selected-eatery').show();
     }, 800);
 }
-
-
-
-// const yelp = require('yelp-fusion');
-// const client = yelp.client('<API KEY>');
-
-// client.search({
-//   term: 'Four Barrel Coffee',
-//   location: 'san francisco, ca',
-// }).then(response => {
-//   console.log(response.jsonBody.businesses[0].name);
-// }).catch(e => {
-//   console.log(e);
-// });
