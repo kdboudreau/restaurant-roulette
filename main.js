61f900a9ef3aef8f31f1d7049fb67cc48d8d0432
Kathryn <kdboudreau1@gmail.com>
2020-02-24 16:51:40 -0500

    changing from zomato to yelp

--- main.js
+++ main.js
@@ -110,3 +110,17 @@ function spinTheWheel() {
         $('.selected-eatery').show();
     }, 800);
 }
+
+
+
+// const yelp = require('yelp-fusion');
+// const client = yelp.client('GZmMnJq0xBxWdtHmpH4-ESYYsljhSHnRxGg6r_NbNkseB_8pHTkkmFvFOcAhB8bYGpZFadW1cGa_KEka02ThfDeqe-8GxwssWTXF2ZGP6g6XkmGXNu4XFpFjGi1UXnYx');
+
+// client.search({
+//   term: 'Four Barrel Coffee',
+//   location: 'san francisco, ca',
+// }).then(response => {
+//   console.log(response.jsonBody.businesses[0].name);
+// }).catch(e => {
+//   console.log(e);
+// });
---
+++ request.js
@@ -0,0 +1,11 @@
+// const yelp = require('yelp-fusion');
+// const client = yelp.client('GZmMnJq0xBxWdtHmpH4-ESYYsljhSHnRxGg6r_NbNkseB_8pHTkkmFvFOcAhB8bYGpZFadW1cGa_KEka02ThfDeqe-8GxwssWTXF2ZGP6g6XkmGXNu4XFpFjGi1UXnYx');
+
+// client.search({
+//   term: 'Four Barrel Coffee',
+//   location: 'san francisco, ca',
+// }).then(response => {
+//   console.log(response.jsonBody.businesses[0].name);
+// }).catch(e => {
+//   console.log(e);
+// });
