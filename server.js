c20977d8b8de8ade3f693057b45fc7cd13504781
Kathryn <kdboudreau1@gmail.com>
2020-08-03 16:00:56 -0400

    some changes to server.js

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+// const http = require('http');
 
-//setting upp cors config
-app.use(cors());
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, resp) => {
+//     resp.statusCode = 200;
+//     resp.setHeader('Content-Type', 'text/plain');
+//     resp.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log('Server running at http://${hostname}:${port}/');
+// });
+
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use('/', express.static(__dirname + '/public'));
+
+app.listen(port);
