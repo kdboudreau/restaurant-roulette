bf2158914d5e190c585b12939e8c9435ea4a0b8c
Kathryn <kdboudreau1@gmail.com>
2020-08-27 12:47:45 -0400

    more structure changes, integrating expess with existing app

--- apiYelpFusion.js
+++ apiYelpFusion.js
@@ -3,20 +3,37 @@ var config = require('./config');
 const apiKey = config.getApiKey();
 
 const searchRequest = {
-  term:'Four Barrel Coffee',
-  location: 'san francisco, ca'
+    term:'Four Barrel Coffee',
+    location: 'san francisco, ca'
 };
 
 const client = yelp.client(apiKey);
 
 client.search(searchRequest).then(response => {
-  const firstResult = response.jsonBody.businesses[0];
-  const prettyJson = JSON.stringify(firstResult, null, 4);
-  const length = response.jsonBody.businesses.length;
-  const index = Math.floor((Math.random() * length) + 1);
-  const randomResult = response.jsonBody.businesses[index];
-  const prettyRandomJson = JSON.stringify(randomResult, null, 4);
-  console.log(randomResult.name);
+    const firstResult = response.jsonBody.businesses[0];
+    const prettyJson = JSON.stringify(firstResult, null, 4);
+    const length = response.jsonBody.businesses.length;
+    const index = Math.floor((Math.random() * length) + 1);
+    const randomResult = response.jsonBody.businesses[index];
+    const prettyRandomJson = JSON.stringify(randomResult, null, 4);
+    console.log(randomResult.name);
 }).catch(e => {
-  console.log(e);
+    console.log(e);
 });
+
+function updateApi() {
+    client.search(searchRequest).then(response => {
+        const firstResult = response.jsonBody.businesses[0];
+        const prettyJson = JSON.stringify(firstResult, null, 4);
+        const length = response.jsonBody.businesses.length;
+        const index = Math.floor((Math.random() * length) + 1);
+        const randomResult = response.jsonBody.businesses[index];
+        const prettyRandomJson = JSON.stringify(randomResult, null, 4);
+        return(randomResult.name);
+    }).catch(e => {
+        console.log(e);
+        return e;
+    });
+}
+
+module.exports = apiYelpFusion;
--- main.js
+++ main.js
@@ -13,69 +13,25 @@
 //     }
 // }
 
-// Giphy API object
-var yelpOld = {
-    url: 'https://api.yelp.com/v3/',
-    api_key: 'GZmMnJq0xBxWdtHmpH4-ESYYsljhSHnRxGg6r_NbNkseB_8pHTkkmFvFOcAhB8bYGpZFadW1cGa_KEka02ThfDeqe-8GxwssWTXF2ZGP6g6XkmGXNu4XFpFjGi1UXnYx'
-};
+const yelpApi = require('apiYelpFusion');
 
-const yelp = require('yelp-fusion');
-var config = require('./config');
-const apiKey = config.getApiKey();
-
-const searchRequest = {
-  term:'Four Barrel Coffee',
-  location: 'san francisco, ca'
-};
-
-const client = yelp.client(apiKey);
-
-// Update trending giphys
 function update() {
     // Toggle refresh state
    $('#update .icon').toggleClass('d-none');
 
-    client.search(searchRequest)
-    .then(response => {
-        const length = response.jsonBody.businesses.length;
-        const index = Math.floor((Math.random() * length) + 1);
-        const randomResult = response.jsonBody.businesses[index];
-        const prettyRandomJson = JSON.stringify(randomResult, null, 4);
-        $(".selected-eatery").html(randomResult.name);
-        console.log(randomResult.name);
-    }).catch(e => {
-        console.log(e);
-    });
+    // client.search(searchRequest)
+    // .then(response => {
+    //     const length = response.jsonBody.businesses.length;
+    //     const index = Math.floor((Math.random() * length) + 1);
+    //     const randomResult = response.jsonBody.businesses[index];
+    //     const prettyRandomJson = JSON.stringify(randomResult, null, 4);
+    //     $(".selected-eatery").html(randomResult.name);
+    //     console.log(randomResult.name);
+    // }).catch(e => {
+    //     console.log(e);
+    // });
+    yelpApi.updateApi();
 
-            // Loop Giphys
-            // $.each( res.data, function (i, giphy) {
-                // Add to latest Giphys
-                // latestGiphys.push(giphy.images.downsized_large.url);
-
-                // Add Giphy HTML
-                // $('#giphys').prepend(
-                //     '<div class="col-sm-6 col-md-4 col-lg-3 p-1">' +
-                //         '<img class="w-100 img-fluid" src="' + giphy.images.downsized_large.url + '">' +
-                //     '</div>'
-                // );
-            // });
-
-            // Inform the SW (if available) of current Giphys
-            // if (navigator.serviceWorker) giphyCacheClean(latestGiphys);
-
-        // // Failure
-        // .fail(function(){
-        //     $('.alert').slideDown();
-        //     setTimeout( function() { $('.alert').slideUp() }, 2000);
-        // })
-
-        // Complete
-        // .always(function() {
-        //     // Re-Toggle refresh state
-        //     // $('#update .icon').toggleClass('d-none');
-        // });
-
-    // Prevent submission if originates from click
     return false;
 }
 
